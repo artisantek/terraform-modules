f461ae61796efaba73d182dda9b564a7f7aeeb60
surekamani <124651854+surekamani@users.noreply.github.com>
2023-03-19 19:54:43 +0530

    Add files via upload

---
+++ dynamodbmo/modmain/vars.tf
@@ -0,0 +1,31 @@
+variable "name" {
+    description = "Enter the Name "
+    type = string
+  
+}
+variable "billing_mode" {
+    description = "Enter the mode "
+   
+  
+}
+variable "read_capacity" {
+    description = "Enter the read "
+   
+  
+}
+variable "write_capacity" {
+    description = "Enter the write "
+
+}
+  variable "hash_key" {
+    description = "Enter the hash "
+   
+  
+}
+variable "name1" {
+    description = "Enter the Name1 "
+   
+  
+}
+
+
