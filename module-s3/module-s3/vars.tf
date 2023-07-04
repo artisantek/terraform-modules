dea63200d56e61ef6dd7b2a4ba410ede510bc61d
venkata-krishna1 <129868933+venkata-krishna1@users.noreply.github.com>
2023-07-04 13:16:06 +0530

    Add files via upload

---
+++ module-s3/module-s3/vars.tf
@@ -0,0 +1,8 @@
+variable "s3_bucket_name" {
+  description = "The name of the bucket to put the file in. Alternatively, an S3 access point ARN can be specified."
+  default     = "white-tub-module"
+}
+
+variable "versioning_enable" {
+    default = "white-tub-module"  
+}  
