26309f2a38dd3f7b1a1438c86e1901c0996fcc8e
Cloud User <ec2-user@ip-172-31-91-12.ec2.internal>
2023-07-04 09:27:57 +0000

    added new changes

--- module-s3/module-s3/vars.tf
+++
@@ -1,8 +0,0 @@
-variable "s3_bucket_name" {
-  description = "The name of the bucket to put the file in. Alternatively, an S3 access point ARN can be specified."
-  default     = "white-tub-module"
-}
-
-variable "versioning_enable" {
-    default = "white-tub-module"  
-}  
